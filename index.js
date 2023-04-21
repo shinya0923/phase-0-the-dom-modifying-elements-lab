dbdbfcbbea520d084b91f8190a4b834df8f22481
Shinya Kudoh <shinya_kd@yahoo.com>
2023-04-20 22:49:30 -0400

    commit tree c6a97e9c06b3cbe3ae783cc5fc54bba53428a3cc

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+newHeader.textContent = ("victory Shinya is the champion");
+ 
